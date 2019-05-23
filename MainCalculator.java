7b73ad5033ab746bff5c5518b39b6dd4a5382d2b
unknown <esmajic@yahoo.com>
2019-05-23 17:33:06 +0200

    added exception in main method in a way that user in the end has to enter correct variable type in order to execute code

--- MainCalculator.java
+++ MainCalculator.java
@@ -2,33 +2,39 @@ import java.util.Scanner;
 
 public class MainCalculator {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		Scanner input = new Scanner(System.in);
-		
-		System.out.println("\n==================================");
-		System.out.println("=       Ervin's Calculator       =");
-		System.out.println("==================================");
 
-		double a, b;
-		System.out.println("\nEnter variable a:  ");
-		a = input.nextDouble();
-		System.out.println("\nEnter variable b:  ");
-		b = input.nextDouble();
+		try {
+			System.out.println("\n==================================");
+			System.out.println("=       Ervin's Calculator       =");
+			System.out.println("==================================");
 
-		CalculatorMethods digitron = new CalculatorMethods();
-		System.out.println();
-		System.out.println("Result of addition is : " + digitron.addition(a, b));
-		System.out.println();
-		System.out.println("Result of subtraction is : " + digitron.subtraction(a, b));
-		System.out.println();
-		System.out.println("Result of multiplication is : " + digitron.multiplication(a, b));
-		System.out.println();
-		System.out.println("Result of division is: " + digitron.division(a, b));
-		System.out.println();
-		System.out.println("Square root result is : " + digitron.sqrt(a));
-		System.out.println();
-		System.out.println("Result of square is : " + digitron.square(a));
+			double a, b;
+			System.out.println("\nEnter variable a:  ");
+			a = input.nextDouble();
+			System.out.println("\nEnter variable b:  ");
+			b = input.nextDouble();
+
+			CalculatorMethods digitron = new CalculatorMethods();
+			System.out.println();
+			System.out.println("Result of addition is : " + digitron.addition(a, b));
+			System.out.println();
+			System.out.println("Result of subtraction is : " + digitron.subtraction(a, b));
+			System.out.println();
+			System.out.println("Result of multiplication is : " + digitron.multiplication(a, b));
+			System.out.println();
+			System.out.println("Result of division is: " + digitron.division(a, b));
+			System.out.println();
+			System.out.println("Square root result is : " + digitron.sqrt(a));
+			System.out.println();
+			System.out.println("Result of square is : " + digitron.square(a));
+
+		} catch (Exception e) {
+			System.out.println("Please, enter correct variable type and try again.");
+			main(args);
+		}
 
 		input.close();
 
